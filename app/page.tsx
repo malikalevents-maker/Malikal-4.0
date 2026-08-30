b02ed49aceb5d83bf7eba5a9c11358dd15069d44
Ramakant <rvalaboj@gitam.in>
2026-08-30 10:49:06 +0530

    Update website

--- app/page.tsx
+++ app/page.tsx
@@ -9,32 +9,23 @@ import Gallery from '../components/Gallery'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
 import LogoIntro from '../components/LogoIntro'
-import ErrorBoundary from '../components/ErrorBoundary'
 
 export default function Home() {
   return (
     <>
-      {/*
-        LogoIntro lives OUTSIDE the ErrorBoundary.
-        If anything inside <main> throws, LogoIntro's timers
-        are unaffected and it will always finish its sequence.
-      */}
       <LogoIntro />
-
-      <ErrorBoundary>
-        <main className="pb-20 md:pb-0">
-          <Header />
-          <EventBanner />
-          <Hero />
-          <About />
-          <Founder />
-          <Services />
-          <UpcomingEvents />
-          <Gallery />
-          <Contact />
-          <Footer />
-        </main>
-      </ErrorBoundary>
+      <main className="pb-20 md:pb-0">
+        <Header />
+        <EventBanner />
+        <Hero />
+        <About />
+        <Founder />
+        <Services />
+        <UpcomingEvents />
+        <Gallery />
+        <Contact />
+        <Footer />
+      </main>
     </>
   )
 }
